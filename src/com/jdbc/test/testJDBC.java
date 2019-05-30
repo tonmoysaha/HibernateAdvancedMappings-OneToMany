c6f1eb991c6cb06d38ca525f53289998189de38e
tonmoysaha <tonmoysahaopi@gmail.com>
2019-05-30 21:13:42 +0600

    initial commit

---
+++ src/com/jdbc/test/testJDBC.java
@@ -0,0 +1,22 @@
+package com.jdbc.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class testJDBC {
+
+	public static void main(String[] args) {
+		String jdbcURL = "jdbc:mysql://localhost:3306/one-to-one-uni?useSSL=false";
+		String uName = "hbstudent";
+		String pass = "hbstudent";
+
+		try {
+			Connection conn = DriverManager.getConnection(jdbcURL, uName, pass);
+			System.out.println("connection Successfully");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
